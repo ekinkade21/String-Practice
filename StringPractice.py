3ed6c58fa0a57ab1fd058b00f5489bbc8b389f1b
ekinkade21 <42971395+ekinkade21@users.noreply.github.com>
2018-10-04 10:22:53 -0400

    Add files via upload

---
+++ StringPractice.py
@@ -0,0 +1,9 @@
+'''
+Created on Oct 4, 2018
+
+@author: ekinkade20
+'''
+firstName=input("Please enter your first name:")
+lastName=input("Please enter your last name:")
+fullName=firstName+' '+lastName
+print(fullName)
